d7df2fe2dfb398e5917c20c83c15879a9a50fa31
Abhinav Kumar <abhinavk.git@gmail.com>
2019-02-04 18:41:06 +0530

    0-1

--- seasonConverter.R
+++ seasonConverter.R
@@ -4,7 +4,7 @@ dates = dataSet$Sampling.Date
 monthColumn = c()
 for(i in seq(1, dim(dataSet)[1])){
   monthColumn[i] = as.integer(strsplit(as.character(dates[i]), "-")[[1]][2])
-  cat("-", monthColumn[i], "\n")
+  #cat("-", monthColumn[i], "\n")
   if(monthColumn[i]>=3 && monthColumn[i] <= 6)
     dataSet$season[i] = "summer"
   else if(monthColumn[i]>=7 && monthColumn[i] <= 9)
@@ -12,5 +12,10 @@ for(i in seq(1, dim(dataSet)[1])){
   else
     dataSet$season[i] = "winter"
   dataSet$quarter[i] = as.integer(monthColumn[i] / 4) + 1
+  
+  if(dataSet$Type.of.Location[i] == "Industrial Area")
+    dataSet$Type.of.Location[i] = 1;
+  if(dataSet$Type.of.Location[i] == "Residential, Rural and other Areas")
+    dataSet$Type.of.Location[i] = 2;
 }
 write.csv(dataSet, file = "pollution.csv")
