21ae2487ca6eae048135cdda66732285e5685a75
Abhinav Kumar <abhinavk.git@gmail.com>
2019-02-28 01:18:29 +0530

    something added

--- generator.R
+++ generator.R
@@ -40,11 +40,32 @@ h_dates = h$dates
     {
       if(j!=2)
       {
-        randombias = sample(seq(4,8), 1)
+        randombias = sample(seq(1,8), 1)
         h[c(1:60,300:365),j] = h[c(1:60,300:365),j] + randombias
       }
     }
-  #}
+  
+#}
 #}
 
 write.csv(h, "health_data_orrisa.csv")
+
+
+
+
+qplot(h$dates[s],h$Nephrology[s], geom='smooth', span =0.00005)
+qplot(datatoprint$dates,datatoprint$no2, geom='smooth', span =50)
+
+
+plot(h$dates[s],h$Nephrology[s],type="l",xlim=c(0, 200), ylim=c(0, 200))
+
+
+####################
+plot(0,0,xlim=c(0, 100), ylim=c(0, 100),main="Yearly cases vs PM level\nRed is PM LEVEL and black is cases", 
+     xlab="Yearly", ylab="Cases and PM level")
+
+lines(datatoprint$dates, datatoprint$pm2.5,col="red")
+lines(h$dates, h$Nephrology)
+h$Nephrology[1:40] = h$Nephrology[1:40]-5
+
+title()
